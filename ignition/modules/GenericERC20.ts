b4021a0df640139d92a7018405e3f180dd871b39
Roderik van der Veer <roderik@settlemint.com>
2024-05-26 16:50:33 +0200

    fix

--- ignition/modules/GenericERC20.ts
+++ ignition/modules/GenericERC20.ts
@@ -1,7 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const GenericERC20Module = buildModule("GenericERC20Module", (m) => {
-  const counter = m.contract("GenericERC20");
+  const counter = m.contract("GenericERC20", ["GenericERC20", "GT"]);
 
   return { counter };
 });
